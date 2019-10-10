3840907bdd50d9be2d4a8354be6dd6188a9c8528
宋旭华 <877340657@qq.com>
2019-10-10 14:28:13 +0800

    添加代码

---
+++ ios/wxgzReactNativeTest/RNManager.h
@@ -0,0 +1,19 @@
+//
+//  RNManager.h
+//  wxgzReactNativeTest
+//
+//  Created by songxuhua on 2019/10/10.
+//  Copyright © 2019 songxuhua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNManager : NSObject
+
++ (void)rnManager;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ ios/wxgzReactNativeTest/RNManager.m
@@ -0,0 +1,17 @@
+//
+//  RNManager.m
+//  wxgzReactNativeTest
+//
+//  Created by songxuhua on 2019/10/10.
+//  Copyright © 2019 songxuhua. All rights reserved.
+//
+
+#import "RNManager.h"
+
+@implementation RNManager
+
++ (void)rnManager {
+    NSLog(@"rnManager");
+}
+
+@end
